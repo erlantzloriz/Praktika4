6c7c21b8673ca373eac0cadb1da01cb3ebe53bfd
Asier Barrio <164157230+AsierBarrio@users.noreply.github.com>
2026-05-08 09:38:21 +0200

    Add files via upload

---
+++ Dropbox.py
@@ -0,0 +1,183 @@
+import requests
+import urllib
+import webbrowser
+from socket import AF_INET, socket, SOCK_STREAM
+import json
+import helper
+
+app_key = ''
+app_secret = ''
+server_addr = "localhost"
+server_port = 8090
+redirect_uri = "http://" + server_addr + ":" + str(server_port)
+
+class Dropbox:
+    _access_token = ""
+    _path = "" # / parece que da problemas
+    _files = []
+    _root = None
+    _msg_listbox = None
+
+    def __init__(self, root):
+        self._root = root
+
+    def local_server(self):
+        # por el puerto 8090 esta escuchando el servidor que generamos
+        server_socket = socket(AF_INET, SOCK_STREAM)
+        server_socket.bind((server_addr, server_port))
+        server_socket.listen(1)
+        print("\tLocal server listening on port " + str(server_port))
+
+        # recibe la redireccio 302 del navegador
+        client_connection, client_address = server_socket.accept()
+        peticion = client_connection.recv(1024)
+        print("\tRequest from the browser received at local server:")
+        print (peticion)
+
+        # buscar en solicitud el "auth_code"
+        primera_linea =peticion.decode('UTF8').split('\n')[0]
+        aux_auth_code = primera_linea.split(' ')[1]
+        auth_code = aux_auth_code[7:].split('&')[0]
+        print ("\tauth_code: " + auth_code)
+
+        # devolver una respuesta al usuario
+        http_response = "HTTP/1.1 200 OK\r\n\r\n" \
+                        "<html>" \
+                        "<head><title>Proba</title></head>" \
+                        "<body>The authentication flow has completed. Close this window.</body>" \
+                        "</html>"
+        client_connection.sendall(http_response.encode('utf-8'))
+        client_connection.close()
+        server_socket.close()
+
+        return auth_code
+
+    def do_oauth(self):
+        #############################################
+        # RELLENAR CON CODIGO DE LAS PETICIONES HTTP
+        # Y PROCESAMIENTO DE LAS RESPUESTAS HTTP
+        # PARA LA OBTENCION DEL ACCESS TOKEN
+        #############################################
+
+        app_key = 'npr0e2xchd7lbo3'
+        app_secret = '97yqlszddw9edu7'
+        if len(app_key) == 0 or len(app_secret) == 0:
+            print(f"falta el app_key o el app_secret en el archivo Dropbox.py\n app_secret:'{app_secret}'\n app_key'{app_key}'")
+            exit(1)
+        servidor = 'www.dropbox.com'
+        params = {'response_type': 'code',
+            'client_id': app_key,
+            'redirect_uri': redirect_uri }
+        params_encoded = urllib.parse.urlencode(params)
+        recurso = '/oauth2/authorize?' + params_encoded
+        uri = 'https://' + servidor + recurso
+        webbrowser.open_new(uri)
+
+        auth_code = self.local_server()
+
+        params = {'code': auth_code,
+        'grant_type': 'authorization_code',
+        'client_id': app_key,
+        'client_secret': app_secret,
+        'redirect_uri': redirect_uri}
+        cabeceras={'User-Agent':'Python Client',
+        'Content-Type': 'application/x-www-form-urlencoded'}
+        uri='https://api.dropboxapi.com/oauth2/token'
+        respuesta = requests.post( uri, headers=cabeceras,data=params)
+        print (respuesta.status_code)
+        json_respuesta = json.loads(respuesta.content)
+        print (json_respuesta)
+        self._access_token = json_respuesta['access_token']
+        print ("\n\nAccess_Token:"+ self._access_token)
+
+        self._root.destroy()
+
+    def list_folder(self, msg_listbox):
+        print("/list_folder")
+        uri = 'https://api.dropboxapi.com/2/files/list_folder'
+        # https://www.dropbox.com/developers/documentation/http/documentation#files-list_folder
+        #############################################
+        # RELLENAR CON CODIGO DE LA PETICION HTTP
+        # Y PROCESAMIENTO DE LA RESPUESTA HTTP
+        #############################################
+        datos = {'path': self._path}
+        datos_encoded = json.dumps(datos)
+        print("Datuak: " + datos_encoded)
+        cabeceras = {'Host': 'api.dropboxapi.com',
+            'Authorization': 'Bearer ' + self._access_token,
+            'Content-Type': 'application/json',
+            'scope': 'files.metadata.read'}
+        respuesta = requests.post(uri, headers=cabeceras, data=datos_encoded,allow_redirects=False)
+        status = respuesta.status_code
+        print ("\tStatus: " + str(status))
+        contenido = respuesta.text
+        print("\tContenido:")
+        contenido_json = json.loads(contenido)
+        print("Ficheros en "+ self._path)
+        for entrie in contenido_json["entries"]:
+            print(entrie['name'])
+
+        self._files = helper.update_listbox2(msg_listbox, self._path, contenido_json)
+
+    def transfer_file(self, file_path, file_data):
+        print("/upload")
+        uri = 'https://content.dropboxapi.com/2/files/upload'
+        # https://www.dropbox.com/developers/documentation/http/documentation#files-upload
+        #############################################
+        # RELLENAR CON CODIGO DE LA PETICION HTTP
+        # Y PROCESAMIENTO DE LA RESPUESTA HTTP
+        #############################################
+        uri = 'https://content.dropboxapi.com/2/files/upload'
+        api_arg = "{\"autorename\":false,\"mode\":\"add\",\"mute\":false,\"path\":\"" + file_path +"\",\"strict_conflict\":false}"
+        cabeceras = {'Host': 'content.dropboxapi.com',
+            'Authorization': 'Bearer ' + self._access_token,
+            'Content-Type': 'application/octet-stream',
+            'scope': 'files.content.write',
+            "Dropbox-API-Arg":  api_arg}
+        
+        respuesta = requests.post(uri, headers=cabeceras, data=file_data)
+        print("\tStatus: " + str(respuesta.status_code))
+        if respuesta.status_code == 200:
+            resultado = json.loads(respuesta.content)
+            print("Archivo subido exitosamente:")
+            print(resultado)
+        else:
+            print("Error al subir el archivo:")
+            print(respuesta.text)
+
+
+    def delete_file(self, file_path):
+        print("/delete_file")
+        uri = 'https://api.dropboxapi.com/2/files/delete_v2'
+        # https://www.dropbox.com/developers/documentation/http/documentation#files-delete
+        #############################################
+        # RELLENAR CON CODIGO DE LA PETICION HTTP
+        # Y PROCESAMIENTO DE LA RESPUESTA HTTP
+        #############################################
+
+    def create_folder(self, path):
+        print("/create_folder")
+        uri = 'https://api.dropboxapi.com/2/files/create_folder_v2'
+       # https://www.dropbox.com/developers/documentation/http/documentation#files-create_folder
+        #############################################
+        # RELLENAR CON CODIGO DE LA PETICION HTTP
+        # Y PROCESAMIENTO DE LA RESPUESTA HTTP
+        #############################################
+        datos = {'path': path,
+                 'autorename': 'false'}
+        datos_encoded = json.dumps(datos)
+        print("Datuak: " + datos_encoded)
+
+        cabeceras = {'Host': 'content.dropboxapi.com',
+            'Authorization': 'Bearer ' + self._access_token,
+            'Content-Type': 'application/json',
+            'scope': 'files.content.write'}
+        respuesta = requests.post(uri, headers=cabeceras, data=datos)
+        print("\tStatus: " + str(respuesta.status_code))
+        if respuesta.status_code == 200:
+            resultado = json.loads(respuesta.content)
+            print("Carpeta creada exitosamente:")
+            print(resultado)
+        else:
+            print("Error al crear la carpeta:")
+            print(respuesta.text)
---
+++ eGela.py
@@ -0,0 +1,311 @@
+# -*- coding: UTF-8 -*-
+from tkinter import messagebox
+import requests
+import urllib
+from urllib.parse import unquote
+from bs4 import BeautifulSoup
+import time
+import helper
+import re
+
+class eGela:
+    _login = 0
+    _cookie = ""
+    _curso = ""
+    _refs = []
+    _root = None
+
+    def __init__(self, root):
+        self._root = root
+
+    def erantzuna_inprimatu(self, metodoa, uri, erantzuna):
+        print("=" * 60)
+        print(f"ESKAERA: {metodoa} {uri}")
+        print(f"ERANTZUNA: {erantzuna.status_code} {erantzuna.reason}")
+        if 'Set-Cookie' in erantzuna.headers:
+            print(f"SET-COOKIE: {erantzuna.headers['Set-Cookie']}")
+        if 'Location' in erantzuna.headers:
+            print(f"LOCATION: {erantzuna.headers['Location']}")
+
+    # Cookie aldatu den konprobatzeko metodoa
+    def cookie_aldaketa(self, erantzuna, aurreko_cookie):
+        if 'Set-Cookie' in erantzuna.headers:
+            return erantzuna.headers['Set-Cookie'].split(';')[0]
+        return aurreko_cookie
+
+    # Helbidea aldatu den konprobatzeko metodoa
+    def helbide_aldaketa(self, erantzuna, uria):
+        if 'Location' in erantzuna.headers:
+            return erantzuna.headers['Location']
+        print(uria)
+        return uria
+
+    def check_credentials(self, username, password, event=None):
+        popup, progress_var, progress_bar = helper.progress("check_credentials", "Logging into eGela...")
+        progress = 0
+        progress_var.set(progress)
+        progress_bar.update()
+
+        print("##### 1. PETICION #####")
+        ################################################################
+        metodo = "GET"
+        uri = "https://egela.ehu.eus/login/index.php"
+        goiburua = { "Host": "egela.ehu.eus",}
+
+        erantzuna = requests.request(metodo, uri, headers=goiburua, allow_redirects=False)
+
+        # if erantzuna.status_code == 200:
+        if 'Set-Cookie' in erantzuna.headers:
+            self._cookie = erantzuna.headers['Set-Cookie'].split(";")[0]
+        if 'Location' in erantzuna.headers:
+            uri = erantzuna.headers['Location']
+
+        html_parser = BeautifulSoup(erantzuna.content, "html.parser")
+        logintoken = html_parser.find("input", {"name": "logintoken"})["value"]
+        ################################################################
+
+        progress = 25
+        progress_var.set(progress)
+        progress_bar.update()
+        time.sleep(1)
+
+        print("\n##### 2. PETICION #####")
+        ################################################################
+        metodo = "POST"
+        goiburua["Cookie"] = self._cookie
+        goiburua["Content-Type"] = "application/x-www-form-urlencoded"
+
+        # username eta password-ean beste balio batzuk agertzen ziren eta metodo hauek erabiliko dira hauen balioak lortzeko
+        user_val = username.get()
+        password_val = password.get()
+
+        edukia = { 'logintoken': logintoken, 'username': user_val, 'password': password_val}
+        edukia_form = urllib.parse.urlencode(edukia)
+        goiburua["Content-Length"] = str(len(edukia_form))
+
+        erantzuna2 = requests.request(metodo, uri, headers=goiburua, data=edukia_form, allow_redirects=False)
+        if 'Set-Cookie' in erantzuna2.headers:
+            self._cookie = erantzuna2.headers['Set-Cookie'].split(";")[0]
+        if 'Location' in erantzuna2.headers:
+            uri = erantzuna2.headers['Location']
+        ################################################################
+
+        progress = 50
+        progress_var.set(progress)
+        progress_bar.update()
+        time.sleep(1)
+
+        print("\n##### 3. PETICION #####")
+        ################################################################
+        metodo = "GET"
+        goiburuak = { "Host": "egela.ehu.eus", "Cookie": self._cookie}
+
+        erantzuna3 = requests.request(metodo, uri, headers=goiburuak, allow_redirects=False)
+
+        if 'Set-Cookie' in erantzuna3.headers:
+            self._cookie = erantzuna3.headers['Set-Cookie'].split(";")[0]
+        if 'Location' in erantzuna3.headers:
+            uri = erantzuna3.headers['Location']
+
+        if goiburuak["Cookie"] != self._cookie:
+            goiburuak["Cookie"] = self._cookie
+        ################################################################
+
+        progress = 75
+        progress_var.set(progress)
+        progress_bar.update()
+        time.sleep(1)
+        popup.destroy()
+
+        print("\n##### 4. PETICION #####")
+        ################################################################
+        erantzuna4 = requests.request(metodo, uri, headers=goiburuak, allow_redirects=False)
+        #profil_url = "https://egela.ehu.eus/user/profile.php"
+        #erantzun_profil = requests.request(metodo, profil_url, headers=goiburuak, allow_redirects=False)
+
+        if erantzuna4.status_code == 200:
+            COMPROBACION_DE_LOG_IN = True
+        # Esto creo que hay que mover abajo
+            html_parser = BeautifulSoup(erantzuna4.text, "html.parser")
+            websis = html_parser.find_all('div', {"class": "card dashboard-card"})
+
+            for w in websis:
+                # <a> esteka bilatzen dugu
+                esteka_probisional = w.find('a')
+                if esteka_probisional:
+                    izena = esteka_probisional.get_text(strip=True)
+                    # Lortutako estekan Web Sistemak dagoela egiaztatu
+                    if "Web Sistemak" in izena:
+                        self._curso = esteka_probisional['href']
+                        print(f"Irakasgaia aurkituta")
+                        print(f"Uria: {self._curso}")
+                        break
+        else:
+            COMPROBACION_DE_LOG_IN = False
+        ################################################################
+
+        progress = 100
+        progress_var.set(progress)
+        progress_bar.update()
+        time.sleep(1)
+        popup.destroy()
+
+        if COMPROBACION_DE_LOG_IN:
+            self._login = 1
+            #############################################
+            # ACTUALIZAR VARIABLES
+            #############################################
+            self._root.destroy()
+        else:
+            messagebox.showinfo("Alert Message", "Login incorrect!")
+
+    def get_pdf_refs(self):
+        popup, progress_var, progress_bar = helper.progress("get_pdf_refs", "Downloading PDF list...")
+        progress = 0
+        progress_var.set(progress)
+        progress_bar.update()
+
+        print("\n##### 4. PETICION (Página principal de la asignatura en eGela) #####")
+        #############################################
+        # RELLENAR CON CODIGO DE LA PETICION HTTP
+        # Y PROCESAMIENTO DE LA RESPUESTA HTTP
+        #############################################
+
+        metodo = 'GET'
+        goiburuak = { "Host": "egela.ehu.eus", "Cookie": self._cookie}
+        print(self._curso + "curso")
+        erantzuna5 = requests.request(metodo, self._curso, headers=goiburuak, allow_redirects=False)
+
+        # print(erantzuna5.content)
+
+        # Esto no se si se debe de dejar o no
+        #progress_step = float(100.0 / len(NUMERO_DE_PDF_EN_EGELA))
+
+        if erantzuna5.status_code == 200:
+            print("\n##### Analisis del HTML... #####")
+        #############################################
+        # ANALISIS DE LA PAGINA DEL AULA EN EGELA
+        # PARA BUSCAR PDFs
+        #############################################
+            html_parser = BeautifulSoup(erantzuna5.text, "html.parser")
+            erlaitz = html_parser.select('ul.nav-tabs .nav-item .nav-link')
+            deskargatutakoak = set()
+
+            # =================================================================
+            # .pdf fitxategiak bilaketa
+            # =================================================================
+            for er in erlaitz:
+                uri_erlaitz = er.get('href')
+
+                if not uri_erlaitz or not uri_erlaitz.startswith('http'):
+                    continue
+
+                # Erlaitzaren izena hartu eta Nabarmenduta zatia kendu
+                gai_izena = er.get_text(strip=True)
+                gaiak = gai_izena.split("Nabarmenduta")
+                gaia = gaiak[0]
+                # Orokorrean fitxategirik izango ez dituzten erlaitzak kenduko dira
+                if any(kendu in gai_izena for kendu in ["Partaideak", "Kalifikazioak", "Gehiago"]):
+                    continue
+
+                uri_erlaitz = er['href']
+                print(f"{gaia} prozesatzen")
+                metodo = 'GET'
+
+                # Fitxategiaren estekan sartuko gara, barnean fitxategi hori deskargatzeko esteka baitago
+                erantzun_fitxategi = requests.request(metodo, uri_erlaitz, headers=goiburuak, allow_redirects=False)
+                html_fitxategi = BeautifulSoup(erantzun_fitxategi.text, "html.parser")
+
+                # Erlaitzean dauden fitxategiak bilatuko dira
+                fitxategiak = html_fitxategi.find_all('li', {"class": "modtype_resource"})
+
+                baimendu_ikastaro = ["Aurkezpena", "Irakaskuntza gida", "Planifikazioa"]
+
+                # Erlaitzeko fitxategiak lortu
+                for fitx in fitxategiak:
+                    f = fitx.find('a')
+                    if not f:
+                        continue
+                    # Fitxategia deskargatzeko esteka
+                    f = f.get('href')
+
+                    fitx_izen = fitx.get_text(strip=True)
+                    # "pdfFitxategia" eta "pyFitxategia" ez ateratzeko egindako garbiketa
+                    fitx_garbi = re.sub(r'(?i)pdf\s+fitxategia|fitxategia', '', fitx_izen).strip()
+
+                    # Ikastaroan dauden fitxategiak deskargatzeko bakarrik
+                    if "Ikastaroa" in gaia:
+                        if not any(baim in fitx_garbi for baim in baimendu_ikastaro):
+                            continue
+
+                    if fitx_garbi in deskargatutakoak:  # Fitxategi bera behin baino gehiagotan ez deskargatzeko
+                        continue
+
+                    # Erdiko eskaera bat, baldin eta estekak beste orri batera eramaten badu
+                    erantzun_dokumentua = requests.request(metodo, f, headers=goiburuak, allow_redirects=False)
+                    html_dok = BeautifulSoup(erantzun_dokumentua.text, "html.parser")
+                    dok = html_dok.find('div', {"class": "resourceworkaround"})
+
+                    # Lortutako esteka beste orri batera eramaten gaituen konprobaketa
+                    dokumentua = ''
+                    if dok:
+                        dokumentua = dok.find('a')
+                        if not dokumentua:
+                            continue
+                        # Orri horretako esteka hartu
+                        dokumentua = dokumentua.get('href')
+
+                    pdf_da = ''
+                    # Lortutako fitxategia pdf edo py denentz egiaztatu
+                    if "pdf" in (dokumentua.lower() or fitx_izen.lower()):
+                        pdf_da = "pdf"
+
+                    if pdf_da:
+                        # Beste esteka batera joan behar denentz konprobaketa
+                        erantzun_dok = ''
+                        if dokumentua:
+                            erantzun_dok = requests.request(metodo, dokumentua, headers=goiburuak, allow_redirects=False)
+                        # Lortutako fitxategiaren luzapenaren arabera balio bat edo bestea ezarri
+                        luzapena = ".pdf"
+
+                        fitxategi_izena = re.sub(r'[\\/*?:"<>|]', "", fitx_garbi)
+
+                        if len(fitxategi_izena) > 100:  # Luzera kontrolatzeko
+                            fitxategi_izena = fitxategi_izena[:100]
+
+                        # Luzapena jarri
+                        if not fitxategi_izena.lower().endswith(luzapena):
+                            fitxategi_izena += luzapena
+
+                        pdf = {'pdf_name': fitx_garbi, 'pdf_link': f}
+                        deskargatutakoak.add(fitx_garbi)
+                        self._refs.append(pdf)
+                        print(f"Fitxategia -> {fitxategi_izena}")
+
+        # INICIALIZA Y ACTUALIZAR BARRA DE PROGRESO
+        # POR CADA PDF ANIADIDO EN self._refs
+
+        NUMERO_DE_PDF_EN_EGELA = len(self._refs)
+        #progress_step = float(100.0 / len(NUMERO_DE_PDF_EN_EGELA))
+
+        # Esto creo que habria que cambiarlo para que poco a poco vaya cargando
+        if self._refs:
+            progress_step = 100 / len(self._refs)
+            for _ in self._refs:
+                progress += progress_step
+                progress_var.set(progress)
+                progress_bar.update()
+                time.sleep(0.1)
+
+        popup.destroy()
+        return self._refs
+
+    def get_pdf(self, selection):
+
+        print("\t##### descargando  PDF... #####")
+        #############################################
+        # RELLENAR CON CODIGO DE LA PETICION HTTP
+        # Y PROCESAMIENTO DE LA RESPUESTA HTTP
+        #############################################
+
+        #return pdf_name, pdf_content
